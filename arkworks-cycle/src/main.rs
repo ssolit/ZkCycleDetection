ee7d82f017e26d346ce5f9881ef76c6479719b9a
Spencer Solit <ssolit@gmail.com>
2023-12-06 16:34:44 -0500

    added ark-serialize

--- arkworks-cycle/src/main.rs
+++ arkworks-cycle/src/main.rs
@@ -1,3 +1,7 @@
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+
 use ark_groth16::{Groth16, prepare_verifying_key};
 use ark_crypto_primitives::snark::{CircuitSpecificSetupSNARK, SNARK};
 use ark_ec::pairing::Pairing;
@@ -10,6 +14,8 @@ use ark_std::{
     rand::{RngCore, SeedableRng},
     test_rng, UniformRand,
 };
+use ark_std::fs::File;
+use ark_serialize::CanonicalSerialize;
 
 
 fn main() {
@@ -47,7 +53,7 @@ impl<ConstraintF: Field> ConstraintSynthesizer<ConstraintF> for MySillyCircuit<C
     }
 }
 
-fn test_prove_and_verify<E>(n_iters: usize)
+fn test_prove_and_verify<E>()
 where
     E: Pairing,
 {
@@ -56,25 +62,31 @@ where
     let (pk, vk) = Groth16::<E>::setup(MySillyCircuit { a: None, b: None }, &mut rng).unwrap();
     let pvk = prepare_verifying_key::<E>(&vk);
 
-    for _ in 0..n_iters {
-        let a = E::ScalarField::rand(&mut rng);
-        let b = E::ScalarField::rand(&mut rng);
-        let mut c = a;
-        c *= b;
 
-        let proof = Groth16::<E>::prove(
-            &pk,
-            MySillyCircuit {
-                a: Some(a),
-                b: Some(b),
-            },
-            &mut rng,
-        )
-        .unwrap();
+    let a = E::ScalarField::rand(&mut rng);
+    let b = E::ScalarField::rand(&mut rng);
+    let mut c = a;
+    c *= b;
 
-        assert!(Groth16::<E>::verify_with_processed_vk(&pvk, &[c], &proof).unwrap());
-        assert!(!Groth16::<E>::verify_with_processed_vk(&pvk, &[a], &proof).unwrap());
-    }
+    let proof = Groth16::<E>::prove(
+        &pk,
+        MySillyCircuit {
+            a: Some(a),
+            b: Some(b),
+        },
+        &mut rng,
+    )
+    .unwrap();
+
+    assert!(Groth16::<E>::verify_with_processed_vk(&pvk, &[c], &proof).unwrap());
+    assert!(!Groth16::<E>::verify_with_processed_vk(&pvk, &[a], &proof).unwrap());
+
+    // let mut compressed_bytes = Vec::new();
+    // proof.serialize_compressed(&mut compressed_bytes).unwrap();
+    // let file_path = "./proof";
+    // let mut file = File::create(file_path);
+    // file.write_all(&compressed_bytes);
+    // file.flush();
 }
 
 mod bls12_381 {
@@ -83,6 +95,6 @@ mod bls12_381 {
 
     #[test]
     fn prove_and_verify() {
-        test_prove_and_verify::<Bls12_381>(100);
+        test_prove_and_verify::<Bls12_381>();
     }
 }
